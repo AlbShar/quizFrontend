6182bfbdd6d742960a9413fc608de760f484403f
AlbShar <sharipov.albert1991@gmail.com>
2022-12-01 20:51:58 +0500

    add firebase

--- src/pages/Quiz/Quiz.jsx
+++ src/pages/Quiz/Quiz.jsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { reference } from '../../index';
+import { onValue} from "firebase/database";
+
 
 const Quiz = () => {
+    useEffect(() => {
+        onValue(reference, (snapshot) => {
+            const data = snapshot.val();
+            console.log(data)
+        })
+    })
     return (
         <main style={{ flex: "1 1 auto" }}>
             hi!
