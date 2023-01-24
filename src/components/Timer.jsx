06091a0632ea77023dd99311621abb188ebede01
AlbShar <sharipov.albert1991@gmail.com>
2023-01-24 18:28:04 +0500

    add pauseTimer function

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -5,28 +5,30 @@ import { StyledDivTimer } from "./styles/Timer.Styled";
 export const deadline = 7200; // seconds
 
 const Timer = () => {
-  let leftSeconds = deadline;
-  const [hours, setHours] = useState("02");
-  const [minutes, setMinutes] = useState("00");
-  const [seconds, setSeconds] = useState("00");
-
+  let [timeLeft, setTimeLeft] = useState(deadline)
   const getFullNumb = (numb) => {
     return `${numb}`.length === 1 ? `0${numb}` : numb;
   };
 
-  const getTime = (leftSeconds) => {
-    setHours(getFullNumb(Math.floor(leftSeconds / 3600) % 60));
-    setMinutes(getFullNumb(Math.floor(leftSeconds / 60) % 60));
-    setSeconds(getFullNumb(Math.floor(leftSeconds % 60)));
+  let hours = getFullNumb(Math.floor(timeLeft / 3600) % 60);
+  let minutes = getFullNumb(Math.floor(timeLeft / 60) % 60);
+  let seconds = getFullNumb(Math.floor(timeLeft % 60));
+  const [isCounting, setIsCounting] = useState(true);
+
+  const pauseTimer = () => {
+    setIsCounting(!isCounting);
   };
 
   useEffect(() => {
-    if (localStorage.getItem('penalty-points')) {
-      localStorage.removeItem('penalty-points');
+    if (localStorage.getItem("penalty-points")) {
+      localStorage.removeItem("penalty-points");
     }
-      const interval = setInterval(() => getTime(leftSeconds >= 1 ? leftSeconds-- : 0), 1000);
-      return () => clearInterval(interval); 
-  }, []); 
+    const interval = setInterval(() => {
+      let isNotification = document.querySelector('#notification') ? true : false;
+        (isCounting) && setTimeLeft(timeLeft >= 1 ? timeLeft-- : 0);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [timeLeft, isCounting]);
 
   return (
     <StyledDivTimer>
@@ -35,7 +37,20 @@ const Timer = () => {
       <span>{minutes}</span>
       <span>:</span>
       <span>{seconds}</span>
-      {(hours === '00' && minutes === '00' && seconds === '00') && <Notification/>}
+      {hours === "00" && minutes === "00" && seconds === "00" && (
+        <Notification
+          title="К сожалению, Ваше время вышло! 😔"
+          subTitle="Продолжайте решать тест дальше. В связи с тем, что время закончилось из Вашего результата вычтется 3 балла."
+        />
+      )}
+      {!isCounting && (
+        <Notification
+          title="Выполнение теста приостановлено"
+          subTitle="Вы можете покинуть эту страницу и продолжить тест в другое время. Ваш прогресс будет сохранен."
+          isCounting={isCounting} setIsCounting={setIsCounting}
+        />
+      )}
+      <button onClick={pauseTimer}>Приостановить тест</button>
     </StyledDivTimer>
   );
 };
