3e718b37e86f0ff0a6dbb153036d94173225fbfd
AlbShar <sharipov.albert1991@gmail.com>
2023-01-24 16:01:50 +0500

    add try...catch for async/await

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import { ref } from "firebase/database";
 import { onValue } from "firebase/database";
 import { deadline } from "./components/Timer";
 
-
 const firebaseConfig = {
   apiKey: "AIzaSyDVNDua7phkjh4mSytFaX6CTuJsImD6Od8",
   authDomain: "quizfrontend-ac9ee.firebaseapp.com",
@@ -24,24 +23,32 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
-
 //#region Functions to get Data from DB(Firebase)
 
-// const getTotalQuestionsNumb = async () => {  
+// const getTotalQuestionsNumb = async () => {
+// try {
 //   return await new Promise(function (resolve, reject) {
 //       onValue(ref(db, `questions`), (snapshot) => {
 //         const totalQuestionsNumb = Object.entries(snapshot.val()).length;
 //         resolve(totalQuestionsNumb);
 //       });
 //     })
+//  }
+// catch(error) {
+// console.log(error);
+//}
 // };
-const getTotalQuestionsNumb = async () => {  
-  return await new Promise(function (resolve, reject) {
+const getTotalQuestionsNumb = async () => {
+  try {
+    return await new Promise(function (resolve, reject) {
       onValue(ref(db, `questions`), (snapshot) => {
         const totalQuestionsNumb = Object.entries(snapshot.val()).length;
         resolve(totalQuestionsNumb);
       });
-    })
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const getAllTestedUsers = () => {
@@ -50,24 +57,31 @@ const getAllTestedUsers = () => {
       const AllTestedUsers = Object.entries(snapshot.val()).length;
       resolve(AllTestedUsers);
     });
-  })
-
-}
+  });
+};
 
 const getAnswersDb = async (currentQuestionNumb) => {
-  let lang = localStorage.getItem('i18nextLng');;
+  try {
+    let lang = localStorage.getItem("i18nextLng");
 
-  return await new Promise(function (resolve, reject) {
-      onValue(ref(db, `answers/answers${currentQuestionNumb}/${lang}`), (snapshot) => {
-        const answersDB = Object.entries(snapshot.val());
-        resolve(answersDB.map((item) => item.join(". ")));
-      });    
-    })
+    return await new Promise(function (resolve, reject) {
+      onValue(
+        ref(db, `answers/answers${currentQuestionNumb}/${lang}`),
+        (snapshot) => {
+          const answersDB = Object.entries(snapshot.val());
+          resolve(answersDB.map((item) => item.join(". ")));
+        }
+      );
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const getThemeQuestion = async (currentQuestionNumb) => {
-  let lang = localStorage.getItem('i18nextLng');
-  return await new Promise(function (resolve, reject) {
+  try {
+    let lang = localStorage.getItem("i18nextLng");
+    return await new Promise(function (resolve, reject) {
       onValue(
         ref(db, `questions/question${currentQuestionNumb}/${lang}/theme`),
         (snapshot) => {
@@ -75,12 +89,16 @@ const getThemeQuestion = async (currentQuestionNumb) => {
           resolve(themQuestion);
         }
       );
-    })
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const getNameQuestion = async (currentQuestionNumb) => {
-  let lang = localStorage.getItem('i18nextLng');
-  return await new Promise(function (resolve, reject) {
+  try {
+    let lang = localStorage.getItem("i18nextLng");
+    return await new Promise(function (resolve, reject) {
       onValue(
         ref(db, `questions/question${currentQuestionNumb}/${lang}/name`),
         (snapshot) => {
@@ -88,59 +106,73 @@ const getNameQuestion = async (currentQuestionNumb) => {
           resolve(nameQuestion);
         }
       );
-    })
-
-}
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const getTotalPointsDB = () => {
-  return (
-    new Promise(function (resolve, reject) {
-      onValue(
-        ref(db, `userAnswers`),
-        (snapshot) => {
-          const answersUser = Object.values(snapshot.val());
-          resolve(answersUser.map(answerUser => answerUser.point).reduce((sum, current) => sum + current, 0))
-        }
+  return new Promise(function (resolve, reject) {
+    onValue(ref(db, `userAnswers`), (snapshot) => {
+      const answersUser = Object.values(snapshot.val());
+      resolve(
+        answersUser
+          .map((answerUser) => answerUser.point)
+          .reduce((sum, current) => sum + current, 0)
       );
-    })
-  )
- };
+    });
+  });
+};
 //#endregion
 
 //#region  Functions for Homepage
 const insertDataInfoTest = async (textQuestions, textTime, ...selectors) => {
-  const textListItems = document.querySelector(selectors[0]).querySelectorAll(selectors[1]);
-  let totalQuestionsNumb = await getTotalQuestionsNumb();
-  
-  textListItems[textListItems.length - 1].textContent = `${totalQuestionsNumb} ${textQuestions}`;
-  // We paste deadline(total seconds for passing the test) at InfoTest component
-  textListItems[0].textContent = `${deadline / 60} ${textTime}`;
+  try {
+    const textListItems = document
+      .querySelector(selectors[0])
+      .querySelectorAll(selectors[1]);
+    let totalQuestionsNumb = await getTotalQuestionsNumb();
+
+    textListItems[
+      textListItems.length - 1
+    ].textContent = `${totalQuestionsNumb} ${textQuestions}`;
+    // We paste deadline(total seconds for passing the test) at InfoTest component
+    textListItems[0].textContent = `${deadline / 60} ${textTime}`;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 // We use the function, when there is html tags in JSON.
 const convertJSONToText = (dataArray) => {
-  dataArray.forEach(data => {
+  dataArray.forEach((data) => {
     if (document.querySelector(data.selector)) {
       document.querySelector(data.selector).innerHTML = JSON.parse(
         JSON.stringify(data.json)
       );
     }
-  })
+  });
 };
 
 const getIdUser = () => {
-  return localStorage.getItem('idUser');
+  return localStorage.getItem("idUser");
 };
 
 const createIdUser = () => {
   const idUser = push(ref(db, `users/user`)).key;
-  localStorage.setItem('idUser', idUser);
+  localStorage.setItem("idUser", idUser);
 };
 
 const setQuiantiyTestedUsers = async (selectorQuantityTestedUsers) => {
-  const quiantiyAllUsers = await getAllTestedUsers();
-  document.querySelector(selectorQuantityTestedUsers).textContent = quiantiyAllUsers;
-}
+  try {
+    const quiantiyAllUsers = await getAllTestedUsers();
+    document.querySelector(selectorQuantityTestedUsers).textContent =
+      quiantiyAllUsers;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 //#endregion
 
@@ -149,19 +181,19 @@ const setQuiantiyTestedUsers = async (selectorQuantityTestedUsers) => {
 const removeAttributesAnswers = (selectorAnswers) => {
   const answersItem = document.querySelectorAll(selectorAnswers);
 
-  answersItem.forEach(answerItem => {
+  answersItem.forEach((answerItem) => {
     if (answerItem.dataset.useranswer) {
-      answerItem.style='';
-      answerItem.removeAttribute('data-useranswer');
+      answerItem.style = "";
+      answerItem.removeAttribute("data-useranswer");
     }
-  })
-
-}
+  });
+};
 
 const getRightAnswerDB = async (currentQuestionNumb) => {
-  let lang = localStorage.getItem('i18nextLng');
+  try {
+    let lang = localStorage.getItem("i18nextLng");
 
-  const rightAnswer = await new Promise(function (resolve, reject) {
+    const rightAnswer = await new Promise(function (resolve, reject) {
       onValue(
         ref(db, `questions/question${currentQuestionNumb}/${lang}/rightAnswer`),
         (snapshot) => {
@@ -172,7 +204,10 @@ const getRightAnswerDB = async (currentQuestionNumb) => {
     });
 
     return rightAnswer;
-}
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const sendUserAnswerDB = async (
   currentQuestionNumb,
@@ -181,70 +216,99 @@ const sendUserAnswerDB = async (
   selectorTheme,
   idUser
 ) => {
-  const theme = document.querySelector(selectorTheme).textContent;
-  const question = document.querySelector(selectorQuestion).textContent;
+  try {
+    const theme = document.querySelector(selectorTheme).textContent;
+    const question = document.querySelector(selectorQuestion).textContent;
 
-  let referenceUserAnswers = ref(db,`users/user${idUser}/answers/answer${currentQuestionNumb}`);
-  const rightAnswer = await getRightAnswerDB(currentQuestionNumb);
+    let referenceUserAnswers = ref(
+      db,
+      `users/user${idUser}/answers/answer${currentQuestionNumb}`
+    );
+    const rightAnswer = await getRightAnswerDB(currentQuestionNumb);
 
-  set(referenceUserAnswers, {
-    question: question,
-    userAnswer: userAnswer,
-    theme: theme,
-    point: rightAnswer === userAnswer ? 1 : 0,
-  });
+    set(referenceUserAnswers, {
+      question: question,
+      userAnswer: userAnswer,
+      theme: theme,
+      point: rightAnswer === userAnswer ? 1 : 0,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-const sendUserInfoDB = async (
-  idUser
-) => {
-  const userName = document.querySelector('#username').value;
-  const userEmail = document.querySelector('#useremail').value;
-  const userAge = document.querySelector('select').value;
-  const userGender = document.querySelector('#userman').checked ? 'man' : document.querySelector('#userwoman').checked ? 'woman' : null;
+const sendUserInfoDB = async (idUser) => {
+  try {
+    const userName = document.querySelector("#username").value;
+    const userEmail = document.querySelector("#useremail").value;
+    const userAge = document.querySelector("select").value;
+    const userGender = document.querySelector("#userman").checked
+      ? "man"
+      : document.querySelector("#userwoman").checked
+      ? "woman"
+      : null;
 
-  const referenceUserAnswers = ref(db,`users/user${idUser}/userInfo`);
-  update(referenceUserAnswers, {
-    name: userName,
-    email: userEmail,
-    age: userAge,
-    gender: userGender,
-  });
+    const referenceUserAnswers = ref(db, `users/user${idUser}/userInfo`);
+    update(referenceUserAnswers, {
+      name: userName,
+      email: userEmail,
+      age: userAge,
+      gender: userGender,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const checkIsUserData = () => {
-  const userName = document.querySelector('#username').value;
-  const userEmail = document.querySelector('#useremail').value;
-  const userAge = document.querySelector('select').value;
-  const userGender = document.querySelector('#userman').checked ? 'man' : document.querySelector('#userwoman').checked ? 'woman' : null;
-  return (userName && userEmail && userAge && userGender) ? true : false;
+  const userName = document.querySelector("#username").value;
+  const userEmail = document.querySelector("#useremail").value;
+  const userAge = document.querySelector("select").value;
+  const userGender = document.querySelector("#userman").checked
+    ? "man"
+    : document.querySelector("#userwoman").checked
+    ? "woman"
+    : null;
+  return userName && userEmail && userAge && userGender ? true : false;
 };
 
-const highlightPreviousAnswer = (idUser, currentQuestionNumb, selectorAnswers) => {
+const highlightPreviousAnswer = (
+  idUser,
+  currentQuestionNumb,
+  selectorAnswers
+) => {
   onValue(
     ref(db, `users/user${idUser}/answers/answer${currentQuestionNumb}`),
     (snapshot) => {
-       setTimeout(() => {
+      setTimeout(() => {
         document.querySelectorAll(selectorAnswers).forEach((answerItem) => {
-            if (answerItem.textContent === snapshot.val().userAnswer) {
-              answerItem.style.border = '2px solid rgb(103, 104, 215)';
-              answerItem.setAttribute('data-useranswer', true);
-            }
-          });
-      }, 1);     
-
-    }, {
-      onlyOnce: true
+          if (answerItem.textContent === snapshot.val().userAnswer) {
+            answerItem.style.border = "2px solid rgb(103, 104, 215)";
+            answerItem.setAttribute("data-useranswer", true);
+          }
+        });
+      }, 1);
+    },
+    {
+      onlyOnce: true,
     }
-  ); 
+  );
 };
 
 const insertTotalQuestionNumbQuiz = async (selector) => {
-  let totalQuestionsNumb = await getTotalQuestionsNumb();
-  document.querySelector(selector).textContent = totalQuestionsNumb;
+  try {
+    let totalQuestionsNumb = await getTotalQuestionsNumb();
+    document.querySelector(selector).textContent = totalQuestionsNumb;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-const insertImageQuiz = (currentQuestionNumb, selectorWrapperImg, selectorImg) => {
+const insertImageQuiz = (
+  currentQuestionNumb,
+  selectorWrapperImg,
+  selectorImg
+) => {
   const wrapperImg = document.querySelector(selectorWrapperImg);
 
   onValue(
@@ -255,14 +319,19 @@ const insertImageQuiz = (currentQuestionNumb, selectorWrapperImg, selectorImg) =
         wrapperImg.style.display = "block";
         document.querySelector(selectorImg).src = imgSrc;
       } else {
-        wrapperImg.style.display = "none"
+        wrapperImg.style.display = "none";
       }
     }
   );
- };
+};
 
-const setWidthScrollBar = async (currentQuestionNumb, selectorQuiz, selectorScrollBar) => {
-  const totalQuestionsNumb = await getTotalQuestionsNumb();
+const setWidthScrollBar = async (
+  currentQuestionNumb,
+  selectorQuiz,
+  selectorScrollBar
+) => {
+  try {
+    const totalQuestionsNumb = await getTotalQuestionsNumb();
     const quiz = document.querySelector(selectorQuiz);
     const widthContainer =
       quiz.clientWidth -
@@ -272,46 +341,59 @@ const setWidthScrollBar = async (currentQuestionNumb, selectorQuiz, selectorScro
     document.querySelector(selectorScrollBar).style.width = `${
       currentQuestionNumb * valueWidthScroll
     }px`;
-
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const insertThemeQuestionQuiz = async (currentQuestionNumb, selector) => {
-  const nameThemeQuestion = await getThemeQuestion(currentQuestionNumb);
-  document.querySelector(selector).textContent =  nameThemeQuestion
+  try {
+    const nameThemeQuestion = await getThemeQuestion(currentQuestionNumb);
+    document.querySelector(selector).textContent = nameThemeQuestion;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const insertNameQuestionQuiz = async (currentQuestionNumb, selector) => {
-  let nameQuestion = await getNameQuestion(currentQuestionNumb);
-  document.querySelector(selector).textContent = nameQuestion;
-
+  try {
+    let nameQuestion = await getNameQuestion(currentQuestionNumb);
+    document.querySelector(selector).textContent = nameQuestion;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-const setAttributesUserAnswer = (e, selectorAnswers, cssBorder, nameDataAtrr) => {
+const setAttributesUserAnswer = (
+  e,
+  selectorAnswers,
+  cssBorder,
+  nameDataAtrr
+) => {
   document.querySelectorAll(selectorAnswers).forEach((item) => {
-    item.style = '';
+    item.style = "";
     item.removeAttribute(nameDataAtrr);
   });
   e.target.style.border = cssBorder;
   e.target.setAttribute(nameDataAtrr, true);
 };
 
-
 //#endregion
 
 //#region Functions for Contact page
 const setAnimateInputAndText = (e, colorFocus) => {
-  e.target.style.transition = 'all ease 0.3s';
+  e.target.style.transition = "all ease 0.3s";
   e.target.style.borderColor = colorFocus;
   e.target.previousElementSibling.style.color = colorFocus;
-}
+};
 
 const clearAnimateInputAndText = (e, colorInitial) => {
   e.target.style.borderColor = colorInitial;
   e.target.previousElementSibling.style.color = colorInitial;
-}
+};
 
 //#endregion
- const checkIsImgAlt = (item, index) => {
+const checkIsImgAlt = (item, index) => {
   try {
     if (!item.alt) {
       throw new Error(
@@ -321,8 +403,7 @@ const clearAnimateInputAndText = (e, colorInitial) => {
   } catch (e) {
     console.error(e);
   }
- };
-
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -330,4 +411,29 @@ root.render(
     <App />
   </React.StrictMode>
 );
-export { db, getTotalQuestionsNumb, setQuiantiyTestedUsers, getAllTestedUsers, setAnimateInputAndText, clearAnimateInputAndText, getIdUser, createIdUser,  getAnswersDb, checkIsUserData, sendUserInfoDB, removeAttributesAnswers, setAttributesUserAnswer, getThemeQuestion, highlightPreviousAnswer, sendUserAnswerDB,  insertDataInfoTest, convertJSONToText, insertNameQuestionQuiz, setWidthScrollBar, insertImageQuiz, insertTotalQuestionNumbQuiz, checkIsImgAlt, insertThemeQuestionQuiz};
+export {
+  db,
+  getTotalQuestionsNumb,
+  setQuiantiyTestedUsers,
+  getAllTestedUsers,
+  setAnimateInputAndText,
+  clearAnimateInputAndText,
+  getIdUser,
+  createIdUser,
+  getAnswersDb,
+  checkIsUserData,
+  sendUserInfoDB,
+  removeAttributesAnswers,
+  setAttributesUserAnswer,
+  getThemeQuestion,
+  highlightPreviousAnswer,
+  sendUserAnswerDB,
+  insertDataInfoTest,
+  convertJSONToText,
+  insertNameQuestionQuiz,
+  setWidthScrollBar,
+  insertImageQuiz,
+  insertTotalQuestionNumbQuiz,
+  checkIsImgAlt,
+  insertThemeQuestionQuiz,
+};
