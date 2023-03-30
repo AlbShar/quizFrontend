32517fd6164ae41ed2f555396841544c56a59ff6
AlbShar <sharipov.albert1991@gmail.com>
2023-03-30 17:20:08 +0500

    Update: improve code at QuantityThemeQuestion module

--- src/modules/QuantityThemeQuestions/QuantityThemeQuestions/QuantityThemeQuestions.tsx
+++ src/modules/QuantityThemeQuestions/QuantityThemeQuestions/QuantityThemeQuestions.tsx
@@ -7,52 +7,65 @@ import {
 } from "./QuantityThemeQuestions.Styled";
 import { ContextQuestionNumb } from "../../../components/Context";
 import Spinner from "../../../UI/Spinner/Spinner";
-import {getThemeQuestion} from "../api/getThemeQuestion";
+import { getThemeQuestion } from "../api/getThemeQuestion";
 import { getTotalQuestionsNumb } from "../../../api/getTotalQuestionsNumb";
 
+type TState = {
+  loading: boolean;
+  questionTheme: string | null;
+  totalQuestionNumber: number;
+};
 const QuantityThemeQuestions: FC = () => {
-  
   const { t } = useTranslation();
   const contextValue: [number, (numb: number) => void] | null =
     useContext(ContextQuestionNumb);
   const currentQuestionNumb = contextValue ? contextValue[0] : 1;
-  const [loading, setLoading] = useState<boolean>(true);
-  const [questionTheme, setQuestionTheme] = useState<string>('');
-  const [totalQuestionNumber, setTotalQuestionNumber] = useState<number>(0);
-  const spinner = <Spinner width={50} height={50} color='#1f2ce0' margin=''/>;
 
-  const loadingIsOver = () => {
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    getThemeQuestion(currentQuestionNumb).then(res => {
-      loadingIsOver();
-      setQuestionTheme(res as string);
-    })
-    getTotalQuestionsNumb().then(res => {
-      if (res !== undefined) {
-        setTotalQuestionNumber(res);
-      }
-    })
-  }, [currentQuestionNumb]);
-
-  const viewQuantity = () => {
+  const [state, setState] = useState<TState>({
+    loading: true,
+    questionTheme: null,
+    totalQuestionNumber: 0,
+  });
+  const view = () => {
     return (
       <StyledH1 id="questionName">
-      {t("Вопрос")} {currentQuestionNumb}
-      <StyledSpanQuestionQuantity id="totalQuestionQuantity" tabIndex={0}>
-        {`/ ${totalQuestionNumber}`}
-      </StyledSpanQuestionQuantity>
-      <StyledSpanThemeQuestion id="themeQuestion">{questionTheme}</StyledSpanThemeQuestion>
-    </StyledH1>
-  
-    )
-  }
+        {t("Вопрос")} {currentQuestionNumb}
+        <StyledSpanQuestionQuantity id="totalQuestionQuantity" tabIndex={0}>
+          {`/ ${state.totalQuestionNumber}`}
+        </StyledSpanQuestionQuantity>
+        <StyledSpanThemeQuestion id="themeQuestion">
+          {state.questionTheme}
+        </StyledSpanThemeQuestion>
+      </StyledH1>
+    );
+  };
+  const spinner = state.loading ? (
+    <Spinner width={50} height={50} color="#1f2ce0" margin="" />
+  ) : null;
+  const content = state.loading ? null : view();
 
-  return (loading ? spinner : viewQuantity());
+  useEffect(() => {
+    Promise.all([
+      getThemeQuestion(currentQuestionNumb),
+      getTotalQuestionsNumb(),
+    ]).then((res) => {
+      const [theme, number] = res;
+      if (theme && number) {
+        setState({
+          loading: false,
+          questionTheme: `${theme}`,
+          totalQuestionNumber: number,
+        });
+      }
+    });
+  }, [currentQuestionNumb]);
+
+  return (
+    <>
+      {spinner}
+      {content}
+    </>
+  );
 };
-
-
 
 export default QuantityThemeQuestions;
