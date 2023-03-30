83f49d61630a4f08fc96a18f0ed86b1e7aeceedb
AlbShar <sharipov.albert1991@gmail.com>
2023-03-30 17:37:18 +0500

    Add spinner at Answers module

--- src/modules/Answers/Answers/Answers.tsx
+++ src/modules/Answers/Answers/Answers.tsx
@@ -4,30 +4,42 @@ import { getAnswersDb } from "../api/getAnswersDb";
 import { StyledArticle, StyledUl } from "./Answers.Styled";
 import Answer from "../components/Answer/Answer";
 import { ContextQuestionNumb } from "../../../components/Context";
+import Spinner from "../../../UI/Spinner/Spinner";
 
+type TState = {
+  loading: boolean;
+  answers: string[];
+};
 const Answers = () => {
-  const [answers, setAnswers] = useState<string[]>([]);
+  const [state, setState] = useState({ answers: [], loading: true });
   const contextValue = useContext(ContextQuestionNumb);
   const currentQuestionNumb = contextValue ? contextValue[0] : 1;
-  const answersItems = answers.map((answer, index) => (
+  const answersItems = state.answers.map((answer, index) => (
     <Answer key={index + 1}>{answer}</Answer>
   ));
-
-  useEffect(() => {
-    removeAttributesAnswers("#answersAll ul li");
-
-    if (currentQuestionNumb) {
-      getAnswersDb(currentQuestionNumb).then((answersDB) =>
-        setAnswers(answersDB as string[])
-      );
-    }
-    // eslint-disable-next-line
-  }, [currentQuestionNumb]);
-
-  return (
+  const spinner = state.loading ? (
+    <Spinner width={50} height={50} color="#1f2ce0" margin="" />
+  ) : null;
+  const content = state.loading ? null : (
     <StyledArticle id="answersAll">
       <StyledUl>{answersItems}</StyledUl>
     </StyledArticle>
+  );
+
+  useEffect(() => {
+    removeAttributesAnswers("#answersAll ul li");
+    if (currentQuestionNumb) {
+      getAnswersDb(currentQuestionNumb).then((answersDB) => {
+        if (Array.isArray(answersDB)) {
+          setState({ answers: answersDB as never[], loading: false });
+        }
+      });
+    }
+  }, [currentQuestionNumb]);
+
+  return (
+   <> {spinner} {content}
+   </>
   );
 };
 
